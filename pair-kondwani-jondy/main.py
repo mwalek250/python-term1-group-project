9c57ec42b96a12d13f19875ece5c4b9c5eb6e682
jondy548 <jondykafusha@mail.com>
2026-02-16 22:52:49 +0200

    [Jondy] Improved final output formatting and added completion message

--- pair-kondwani-jondy/main.py
+++ pair-kondwani-jondy/main.py
@@ -7,3 +7,11 @@ else:
     print("Odd")
 
 number_analyzer()
+# Prime or Not
+    if is_prime(number):
+        print("It is a Prime number 🟢")
+    else:
+        print("It is Not a Prime number 🔴")
+
+    # End message
+    print("\n✅ Analysis Complete. Thank you for using Number Analyzer!")
