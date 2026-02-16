7c7a915e1e3e99d467a8389c3f61eb74e1de7c73
jondy548 <jondykafusha@mail.com>
2026-02-16 22:40:04 +0200

    [Jondy] Added positive/negative check

--- pair-kondwani-jondy/main.py
+++ pair-kondwani-jondy/main.py
@@ -1,12 +1,9 @@
-# Number Analyzer — even/odd only
 
-def number_analyzer():
-    number = int(input("Enter a number: "))
-
-    if number % 2 == 0:
-        print(f"{number} is Even")
-    else:
-        print(f"{number} is Odd")
-
+# Basic even/odd checker
+number = int(input("Enter a number: "))
+if number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
 
 number_analyzer()
