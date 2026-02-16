ebf9c66be7a204ee6ed610589419d24495a84e9d
mwalek250 <mwalek250@gmail.com>
2026-02-16 21:14:42 +0200

    [Kondwani] Added basic even/odd checker

---
+++ pair-kondwani-jondy/main.py
@@ -0,0 +1,12 @@
+# Number Analyzer — even/odd only
+
+def number_analyzer():
+    number = int(input("Enter a number: "))
+
+    if number % 2 == 0:
+        print(f"{number} is Even")
+    else:
+        print(f"{number} is Odd")
+
+
+number_analyzer()
